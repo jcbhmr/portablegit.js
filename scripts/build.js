68f2e4cc3707ec1b8125cb2185d0a789a4b62b0d
null <null>
2024-02-22 13:31:19 -0600

    save

---
+++ scripts/build.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import { $ } from "execa"
+import { readFile, mkdir } from "node:fs/promises"
+import { createWriteStream } from "node:fs"
+import { resolve, dirname } from "node:path"
+import { Writable } from "node:stream"
+
+const packageText = await readFile("./package.json", "utf8")
+const package_ = JSON.parse(packageText)
+
+const [, portableGitVersion] = package_.version.split("+")
+const gitVersion = portableGitVersion.match(/^\d+\.\d+\.\d+/)[0]
+
+const filename = `PortableGit-${gitVersion}-64-bit.7z.exe`
+const url = `https://github.com/git-for-windows/git/releases/download/v${portableGitVersion}/${filename}`
+console.error(`%cDownloading%c from %o`, "color:lime;font-weight:bold", "", url)
+const response = await fetch(url)
+const archivePath = resolve(`out/${filename}`)
+await mkdir(dirname(archivePath), { recursive: true })
+const writable = Writable.toWeb(createWriteStream(archivePath))
+await response.body.pipeTo(writable)
+
+const portableGitRoot = resolve(`out/PortableGit`)
+await mkdir(portableGitRoot, { recursive: true })
+await $({
+    stdio: "inherit",
+    cwd: portableGitRoot,
+})`7z x -aos ${archivePath}`
