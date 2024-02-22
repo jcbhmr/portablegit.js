a41376ddfad19e3a66f81fb150800f07a4247167
Jacob Hummer <jcbhmr@outlook.com>
2024-02-22 14:38:52 -0600

    Update gh-release-create.js

--- scripts/gh-release-create.js
+++ scripts/gh-release-create.js
@@ -7,11 +7,11 @@ const packageText = await readFile("./package.json", "utf8")
 const package_ = JSON.parse(packageText)
 const octokit = github.getOctokit(process.env.GH_TOKEN)
 
-const currentVersionRelease = (await octokit.rest.repos.getReleaseByTag({
+const currentVersionRelease = await octokit.rest.repos.getReleaseByTag({
     owner: process.env.GITHUB_REPOSITORY.split("/")[0],
     repo: process.env.GITHUB_REPOSITORY.split("/")[1],
     tag: `v${package_.version}`
-})).catch(() => null)
+}).catch(() => null)
 if (!currentVersionRelease) {
     const newTag = `v${package_.version}`
     await $`gh release create ${newTag} --generate-notes`
