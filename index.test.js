0f7106422087d0c5b6f7e41866a45107c5a68337
Jacob Hummer <jcbhmr@outlook.com>
2024-05-18 18:01:05 -0500

    save

--- index.test.js
+++ index.test.js
@@ -2,6 +2,7 @@ import test from "node:test";
 import assert from "node:assert/strict";
 import { $ } from "execa";
 import { readFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
 const package_ = JSON.parse(
   await readFile(new URL(import.meta.resolve("./package.json")), "utf8"),
 );
@@ -9,7 +10,7 @@ const package_ = JSON.parse(
 await $({ stdio: "inherit" })`npm run build`;
 // simulate postinstall
 await $({
-  cwd: "out",
+  cwd: fileURLToPath(import.meta.resolve("./out/")),
   stdio: "inherit",
   reject: false,
 })`post-install.bat`;
@@ -22,7 +23,9 @@ test("bash", async () => {
   await $({ stdio: "inherit" })`${package_.bin.bash} --version`;
 });
 
-test("import.meta.resolve()", () => {
-    console.log(import.meta.resolve("portablegit/usr/bin/cat"))
-    console.log(import.meta.resolve("portablegit/usr/bin/cat.exe"))
+test("import.meta.resolve() cat", async () => {
+    const cat = fileURLToPath(import.meta.resolve("portablegit/usr/bin/cat"))
+    await $({ stdio: "inherit" })`${cat} --version`
+    const catExe = fileURLToPath(import.meta.resolve("portablegit/usr/bin/cat.exe"))
+    await $({ stdio: "inherit" })`${catExe} --version`
 })
--- scripts.js
+++ scripts.js
@@ -14,10 +14,11 @@ async function generate() {
   console.debug("url=%o", url);
   const response = await fetch(url);
   assert(response.status === 200, `${response.status} ${response.url}`);
+  const dest = new URL(import.meta.resolve("./PortableGit-64-bit.7z.exe"))
   await response.body.pipeTo(
-    Writable.toWeb(createWriteStream("PortableGit-64-bit.7z.exe")),
+    Writable.toWeb(createWriteStream(dest)),
   );
-  console.log("Downloaded %o to %o", url, "PortableGit-64-bit.7z.exe");
+  console.log("Downloaded %s to %s", url, dest);
 }
 
 // POSTINSTALL RUNS WITH NO DEV DEPENDENCIES!
@@ -25,14 +26,15 @@ async function postinstall() {
   const { $ } = await import("execa");
   const { existsSync } = await import("node:fs");
   const { sep } = await import("node:path")
+  const { fileURLToPath } = await import("node:url")
 
   if (process.env.INIT_CWD === process.cwd() || process.env.INIT_CWD?.startsWith(process.cwd() + sep)) {
     // Skip! Local dev `npm install`, not as a dep.
   } else {
     // May have already been installed once and had the files deleted.
-    if (existsSync("out/post-install.bat")) {
+    if (existsSync(new URL(import.meta.resolve("./out/post-install.bat")))) {
       await $({
-        cwd: "out",
+        cwd: fileURLToPath(import.meta.resolve("./out/")),
         stdio: "inherit",
         reject: false,
       })`post-install.bat`;
